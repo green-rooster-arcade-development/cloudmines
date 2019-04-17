3459f954ccf0a07e356f01f93b6b3cecb0b7c766
Kevin Cromwell <kevincromwell@Andrews-MacBook-Air.local>
2019-04-16 19:41:31 -0500

    Revert "Add files via upload"
    This reverts commit dc06dbf9c38eefe607fd3c6739ab9bb745569d74.

--- cloudmines.py
+++ cloudmines.py
@@ -2,33 +2,6 @@ import pygame
 import random
 from pygame.locals import *
 
-
-
-class Machine():
-    """
-    THIS CLASS IS THE STATE MACHINE FOR THE APPLICATION
-    """
-    def __init__(self):
-        self.transitions = {}
-        self.state = 'pause'
-
-    def addTransition(self,**kwargs):
-        if kwargs['name'] in self.transitions:
-            self.transitions[kwargs['name']]['states'].append(kwargs['state'])
-        else:
-            self.transitions[kwargs['name']] = {'states':[kwargs['state']]}
-            #self.transitions[kwargs['name']]['states'].add(kwargs['state'])
-            print(self.transitions[kwargs['name']]['states'])
-#            self.transitions[kwargs['name']]['states'] = [kwargs['state']]
-
-    def update(self,state):
-        if self.transitions[state] and self.state in self.transitions[state]['states']:
-            self.state = state
-            print(self.state)
-        else:
-            # error states
-            pass
-
 class Robot(pygame.sprite.Sprite):
     """
     THIS CLASS REPRESENTS THE PLAYER
@@ -42,23 +15,18 @@ class Robot(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 60
         self.rect.y = 40
-        self.direction = ''
-
-    def handleKeyPress(self,key):
-        if key == K_UP:
-            self.direction = 'u'
-        elif key == K_DOWN:
-            self.direction = 'd'
-        else:
-            self.direction = ''
+        self.direction = 'r'
 
     def update(self):
-        if app_machine.state == 'forward':
-            self.image = pygame.image.load("./resources/robot_right.png")
-        if app_machine.state == 'backward':
-            self.image = pygame.image.load("./resources/robot_left.png")
+        pass
 
     def set_direction(self,d):
+        if d == 'l':
+            self.image = pygame.image.load("./resources/robot_left.png")
+            self.direction = 'l'
+        if d == 'r':
+            self.image = pygame.image.load("./resources/robot_right.png")
+            self.direction = 'r'
         if d == 'u':
             self.rect.y -= 5
         if d == 'd':
@@ -79,8 +47,8 @@ class Mine(pygame.sprite.Sprite):
         self.boom = pygame.mixer.Sound('./resources/explode.wav')
 
     def update(self):
-        self.rect.x += 1 if app_machine.state == 'backward' else -1
-        if self.rect.x < -20 or app_machine.state == 'pause':
+        self.rect.x -= 1
+        if self.rect.x < -20:
             self.kill()
 
     def explode(self):
@@ -96,7 +64,7 @@ class Laser(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.Surface((width,height))
         self.image.fill(color)
-        self.direction = 'l' if app_machine.state == 'backward' else 'r'
+        self.direction = robot.direction
         self.rect = self.image.get_rect()
         self.rect.x = robot.rect.x + 40
         self.rect.y = robot.rect.y + 40
@@ -111,17 +79,6 @@ class Laser(pygame.sprite.Sprite):
         if self.rect.x < -9 or self.rect.x > 243:
             self.kill()
 
-app_machine = Machine()
-app_machine.addTransition(name='forward', state='off')
-app_machine.addTransition(name='forward', state='pause')
-app_machine.addTransition(name='forward', state='backward')
-app_machine.addTransition(name='backward', state='forward')
-app_machine.addTransition(name='backward', state='pause')
-app_machine.addTransition(name='off', state='forward')
-app_machine.addTransition(name='off', state='backward')
-app_machine.addTransition(name='off', state='pause')
-app_machine.addTransition(name='pause', state='forward')
-app_machine.addTransition(name='pause', state='backward')
 
 def main():
 
@@ -131,6 +88,7 @@ def main():
     logo = pygame.image.load("./resources/scroll_bg.png")
     pygame.display.set_icon(logo)
     pygame.display.set_caption("Cloud Mines")
+
 
     # Initialize the screen
     screen = pygame.display.set_mode((244,280))
@@ -157,51 +115,42 @@ def main():
     # Main loop
     while running:
 
+        # event handling - get all the events from the queue
         for event in pygame.event.get():
+            # do something when the event is QUIT
             if event.type == pygame.QUIT:
-                app_machine.update('end')
+                # change the loop value to false
+                running = False
             elif event.type == KEYDOWN:
-                if event.key == K_LEFT:
-                    app_machine.update('backward')
-                elif event.key == K_RIGHT:
-                    app_machine.update('forward')
-                elif event.key == K_s:
-                    app_machine.update('off')
-                elif event.key == K_p:
-                    app_machine.update('pause')
-                elif event.key == K_SPACE:
-                    laser = Laser((255,0,0),10,10,robot1)
-                    laser_sprites_list.add(laser)
-                    all_sprites_list.add(laser)
+                    if event.key == K_UP:
+                        robot1.set_direction('u')
+                    elif event.key == K_DOWN:
+                        robot1.set_direction('d')
+                    elif event.key == K_LEFT:
+                        robot1.set_direction('l')
+                    elif event.key == K_RIGHT:
+                        robot1.set_direction('r')
+                    elif event.key == K_SPACE:
+                        laser = Laser((255,0,0),10,10,robot1)
+                        laser_sprites_list.add(laser)
+                        all_sprites_list.add(laser)
 
-                robot1.handleKeyPress(event.key)
+        if pygame.key.get_pressed()[K_UP]:
+            robot1.set_direction('u')
 
-        if app_machine.state == 'forward' or app_machine.state == 'backward':
-        # handle thrusters
-            if pygame.key.get_pressed()[K_UP]:
-                robot1.set_direction('u')
-            if pygame.key.get_pressed()[K_DOWN]:
-                robot1.set_direction('d')
-        # drop mines
-            if (release_timer + 2000) < pygame.time.get_ticks():
-                if app_machine.state == 'forward':
-                    release_timer = pygame.time.get_ticks()
-                    mine = Mine((0,0,255),20,20)
-                    mine_sprites_list.add(mine)
-                    all_sprites_list.add(mine)
-        # update screen scroll
-            bgx += 1 if app_machine.state == 'backward' else -1
-            if bgx < -468:
-                bgx = 0
-            if bgx == 1:
-                bgx = -468
+        if pygame.key.get_pressed()[K_DOWN]:
+            robot1.set_direction('d')
 
-        elif app_machine.state == 'pause':
-            pass
-        elif app_machine.state == 'off':
-            running = False
-        else:
-            pass
+        bgx += 1 if robot1.direction == 'l' else -1;
+        if bgx < -468:
+            bgx = 0
+        if bgx == 1:
+            bgx = -468
+        if (release_timer + 2000) < pygame.time.get_ticks():
+            mine = Mine((0,0,255),20,20)
+            mine_sprites_list.add(mine)
+            all_sprites_list.add(mine)
+            release_timer = pygame.time.get_ticks()
 
 
         shot_mine_list = pygame.sprite.groupcollide(laser_sprites_list, mine_sprites_list, True, True)
@@ -216,9 +165,8 @@ def main():
                 lives = lives - 1
 
         if lives <= 0:
-            app_machine.update('pause')
-            #robot1.kill()
-            score = 0
+            robot1.kill()
+            score = 'Game Over'
 
         screen.blit(logo,(bgx,0))
 
@@ -226,12 +174,7 @@ def main():
         all_sprites_list.draw(screen)
 
         basicfont = pygame.font.SysFont(None, 40)
-        if app_machine.state == 'off':
-            text = basicfont.render('Game Over', True, (0,255,0), (0, 0, 0))
-        elif app_machine.state == 'pause':
-            text = basicfont.render('Paused', True, (0,255,0), (0, 0, 0))
-        else:
-            text = basicfont.render('%s'%score, True, (0,255, 0), (0, 0, 0))
+        text = basicfont.render('%s'%score, True, (0,255, 0), (0, 0, 0))
         textrect = text.get_rect()
         textrect.centerx = screen.get_rect().centerx
         textrect.y = 240
